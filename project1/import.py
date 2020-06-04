1751cde6aeca6938b6b10b6aca643c19aca18820
David Vann <dv6bq@virginia.edu>
2020-06-04 19:12:56 -0400

    Finished import.py to add all of the books to a PSQL database

---
+++ project1/import.py
@@ -0,0 +1,20 @@
+import csv
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine(os.getenv("DATABASE_URL")) # need to set environment variable in terminal with "export DATABASE_URL=...(the actual URL of the database)"
+
+db = scoped_session(sessionmaker(bind=engine))
+
+f = open("books.csv")
+
+reader = csv.reader(f)
+next(reader, None)
+
+for isbn, title, author, year in reader:
+    db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
+    {"isbn": isbn, "title": title, "author": author, "year": year})
+    print(f"Added book. ISBN: {isbn}; Title: {title}, Author: {author}, Year: {year}.")
+db.commit()
