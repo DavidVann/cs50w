15a7ba1a128fb5a9f7d19cf33e5a14d98e4fbfa7
David Vann <dv6bq@virginia.edu>
2020-06-12 04:11:55 -0400

    Can now search for books; reviews show up for books properly (mostly, the timestamp isn't very pretty)

--- project1/application.py
+++ project1/application.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 from werkzeug.security import check_password_hash, generate_password_hash
 
+
 app = Flask(__name__)
 
 # Check for environment variable
@@ -21,6 +22,17 @@ Session(app)
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
+
+
+# Helper functions
+def get_reviews(book_id):
+    reviews = db.execute(
+        'SELECT  username, review_score, review_text, time, book_id FROM users JOIN reviews \
+        ON reviews.user_id = users.id WHERE book_id=:book_id',
+        {"book_id":book_id}
+    )
+    
+    return reviews
 
 
 @app.route("/")
@@ -45,7 +57,8 @@ def register():
 
         if error is None:
             db.execute(
-                'INSERT INTO users (username, password) VALUES (:username, :password)', {"username":username, "password":generate_password_hash(password)}
+                'INSERT INTO users (username, password) VALUES (:username, :password)', 
+                {"username":username, "password":generate_password_hash(password)}
             )
             db.commit()
             return redirect(url_for('login'))
@@ -74,7 +87,7 @@ def login():
             session.clear()
             session['user_id'] = user['id']
             session['username'] = user['username']
-            return redirect(url_for('search'))
+            return redirect(url_for('index'))
 
         flash(error)
     
@@ -85,5 +98,61 @@ def logout():
     session.clear()
     return redirect(url_for('index'))
 
-@app.route("/search")
+@app.route("/search", methods=('GET', 'POST'))
 def search():
+    if request.method == 'POST':
+        query = request.form['query']
+        
+        query = query.title()
+        query = "%" + query + "%"
+        
+        results = db.execute(
+            "SELECT * FROM books WHERE isbn LIKE :query OR title LIKE :query OR author LIKE :query",
+            {"query":query}
+        ).fetchall()
+    
+    if results is None:
+        flash("No books match that description.")
+        
+    return render_template('search.html', results=results)
+    
+@app.route("/book/<isbn>", methods=('GET', 'POST'))
+def book_reviews(isbn):
+    book = db.execute(
+        'SELECT * FROM books WHERE isbn = :isbn',
+        {"isbn":isbn}
+    ).fetchone()
+    
+    book_id = book[0]
+    
+    reviews = get_reviews(book_id)
+    
+    # Code for writing a new review
+    if request.method == 'POST':
+        if session.get('username') is None:
+            flash("Must be logged in to write a review.")
+        else:
+            already_reviewed = db.execute(
+                'SELECT * FROM reviews WHERE user_id = :user_id AND book_id = :book_id',
+                {"user_id":session['user_id'], "book_id":book_id}
+            ).fetchone()
+            
+            if already_reviewed is not None:
+                flash("Can only review a book one time.")
+            else:
+                review_score = request.form['review_score']
+                review_text = request.form['review_text']
+                
+                db.execute(
+                    'INSERT INTO reviews (user_id, book_id, review_score, review_text) \
+                    VALUES(:user_id, :book_id, :review_score, :review_text)',
+                    {"user_id":session['user_id'], "book_id":book_id, "review_score":int(review_score), "review_text":review_text}
+                )
+                db.commit()
+                
+                return redirect('/book/' + isbn)
+            
+    
+    
+    return render_template('book.html', book=book, reviews=reviews)
+    
